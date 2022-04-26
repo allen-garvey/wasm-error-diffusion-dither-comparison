4b0cf93e41467b13e4e225a4545702442ccb96c1
Allen Garvey <allengarvey@hotmail.com>
2022-04-26 10:11:04 -0400

    Add webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+            `${__dirname}/src/index.js`, 
+            `${__dirname}/sass/style.scss`,
+        ],
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public/assets'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+            watch: true,
+        },
+        devMiddleware: {
+            publicPath: '/assets/'
+        },
+        port: 3000,
+        open: true,
+        client: {
+            overlay: {},
+        },
+        historyApiFallback: {
+            index: 'index.html'
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.scss$/,
+                oneOf: [
+                    // this matches `<style module>`
+                    {
+                        resourceQuery: /module/,
+                        use: [
+                            'vue-style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    esModule: false,
+                                    modules: {
+                                        localIdentName: '[local]_[hash:base64:8]',
+                                    },
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                            },
+                        ]
+                    },
+                    {
+                        use: [
+                            'vue-style-loader',
+                            {
+                                loader: MiniCssExtractPlugin.loader,
+                            },
+                            'css-loader',
+                            'sass-loader',
+                        ]
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: './style.css',
+        }),
+    ],
+};
