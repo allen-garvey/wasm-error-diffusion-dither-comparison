226180ef977f0acb7f054b561026b4d4a2cc09e5
Allen Garvey <allengarvey@hotmail.com>
2022-04-26 11:00:58 -0400

    Add initial vue component

---
+++ src/components/app.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :class="$style.container">
+        <div>
+            <input 
+                type="file" 
+                accept="image/png,image/gif,image/jpeg,image/webp"
+            />
+        </div>
+        <div>
+            <canvas ref="outputCanvas"></canvas>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" module>
+.container {
+
+}
+</style>
+
+<script>
+export default {
+    components: {
+    },
+    created(){
+    },
+    data(){
+        return {
+        };
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+    }
+};
+</script>
---
+++ src/message-headers.js
@@ -0,0 +1,8 @@
+export const messageHeaders = {
+    IMAGE_LOAD:     1,
+    DITHER_JS:      2,
+    DITHER_C:       3,
+    DITHER_D:       4,
+    DITHER_RUST:    5,
+    DITHER_ZIG:     6,
+};
