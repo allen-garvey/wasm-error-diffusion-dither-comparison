9aa331581aa792dcbf8b7fd6d1855d86cc199ebb
Allen Garvey <allengarvey@hotmail.com>
2022-05-09 08:38:36 -0400

    Fix sass locals console error

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,6 @@ module.exports = {
                     },
                     {
                         use: [
-                            'vue-style-loader',
                             {
                                 loader: MiniCssExtractPlugin.loader,
                             },
