3d5479bd04dd54a9c4fa9adb1258ef12b0ed3e91
Allen Garvey <allengarvey@hotmail.com>
2022-05-11 14:39:48 -0400

    Fix wasm error buffer not being allocated enough memory

--- src/worker/wasm.js
+++ src/worker/wasm.js
@@ -23,10 +23,10 @@ const initializeWasm = (memo) => Promise.all(wasmModel.map(({key, source}) =>
 const instantiateWasm = (memo, imageWidth, imageHeight) => {
     const PAGE_SIZE_IN_BYTES = 64 * 1024;
     const imageSizeInPages = Math.ceil(imageWidth * imageHeight * 4 / PAGE_SIZE_IN_BYTES);
-    // enough space to store 3 rows of 2 byte shorts
+    // enough space to store 3 rows of 4 byte floats, with 2 item buffer on each side
     const NUM_ERROR_ROWS = 3;
     const ERROR_ARRAY_ITEM_BYTE_SIZE = 4; //float32
-    const errorBufferSizeInPages = Math.ceil(imageWidth * ERROR_ARRAY_ITEM_BYTE_SIZE * NUM_ERROR_ROWS / PAGE_SIZE_IN_BYTES);
+    const errorBufferSizeInPages = Math.ceil((imageWidth + 4) * ERROR_ARRAY_ITEM_BYTE_SIZE * NUM_ERROR_ROWS / PAGE_SIZE_IN_BYTES);
     const memoryPagesRequired = imageSizeInPages + errorBufferSizeInPages;
     
     return Promise.all(Object.keys(memo).map((key) => {
